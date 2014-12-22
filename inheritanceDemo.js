438adb7afcc49d38e1a4d46aed5eb4853565f508
Amit <amit2.kumar@paytm.com>
2014-12-22 13:39:50 +0530

    Calling parent constructor into child constructor.

--- inheritanceDemo.js
+++ inheritanceDemo.js
@@ -9,6 +9,7 @@ LivingThing.prototype.move = function () {
 };
 
 function Bird() {
+    LivingThing.call(this);
 }
 Bird.prototype = Object.create(LivingThing.prototype);
 Bird.prototype.constructor = Bird;
@@ -17,6 +18,7 @@ Bird.prototype.fly = function () {
 };
 
 function Peacock() {
+    Bird.call(this);
 }
 Peacock.prototype = Object.create(Bird.prototype);
 Peacock.prototype.constructor = Peacock;
@@ -24,7 +26,7 @@ Peacock.prototype.dance = function () {
     console.log("I am Peacock! I can dance");
 };
 
-var peacock = new Peacock("A");
+var peacock = new Peacock();
 peacock.dance(); // I am Peacock! I can dance
 peacock.fly(); // I am bird! I can fly!!
 peacock.move(); // I am living thing! I can move!!
